14012e802f5f0781a31b6b7a299b12914302ad00
Dinesh5aini <dineshsaini8702@gmail.com>
2023-08-20 19:21:23 +0530

    elrwlr

--- numpy project #1.py
+++ numpy project #1.py
@@ -36,17 +36,23 @@ print("Dimension of array:",dim)
 shape=np.shape(temprature)
 print("Shape of array:",shape)
 
+print()
+
 '''
 3. Print the daily temperatures for the first week of each month.
 '''
-daily_temp_1st_week=temprature[:,::4,:,:]
+daily_temp_1st_week=temprature[:,0,:,:]
 print("Daily temprature of 1st week:",daily_temp_1st_week)
+
+print()
 
 '''
 4. Print the temperatures for Tuesday of each month.
 '''
-tuesday_temp=temprature[:,:,1:2,:]
+tuesday_temp=temprature[:,:,1,:]
 print("Tempratures for tuesday:",tuesday_temp)
+
+print()
 
 '''
 5. Print only the maximum temperature for all the weekdays of Dec and Feb.
@@ -55,7 +61,83 @@ print("Tempratures for tuesday:",tuesday_temp)
 max_temp_allDays_dec_feb=temprature[1::2,:,:,1]
 print("Only max. temp. of all days of dec and feb:",max_temp_allDays_dec_feb)
 
+print()
+
 '''
 6. Print all the days along with the week number in November when the
 minimum temperature was less than 8 degrees.
 '''
+
+nov_min=temprature[0,:,:,0]
+nov_min_lt8=np.where(nov_min<8)
+print("The days along with the week number in November \nwhen the minimum temperature was less than 8 degrees:")
+for day,week in np.nditer([nov_min_lt8[1],nov_min_lt8[0]]):
+    if day==0:
+        print(f"Monday of week ",week+1)
+    elif day==1:
+        print(f"Tuesday of week ",week+1)
+    elif day==2:
+        print(f"Wednesday of week ",week+1)
+    elif day==3:
+        print(f"Thursday of week ",week+1)
+    elif day==4:
+        print(f"Friday of week ",week+1)
+    elif day==5:
+        print(f"Saturday of week ",week+1)
+    else :
+        print(f"Sunday of week ",week+1)
+    
+print()
+
+'''
+7. Print all the weeks in Dec and Jan where the maximum temperature has
+crossed a threshold of 20 degrees.
+'''
+
+dec_jan_max=temprature[1:3,:,:,1]
+max_temp_gt20=np.where(dec_jan_max>20)
+print("The weeks in Dec and Jan where the maximum\n temperature has crossed a threshold of 20 degrees:")
+
+for month,week in np.nditer([max_temp_gt20[0],max_temp_gt20[1]]):
+    if month==0:
+        print(f"week {week+1} of December")
+    else:
+        print(f"week {week+1} of December ")
+
+print()
+
+'''
+8. Check if there are any absurd values present in the dataset(like some temp
+which should not be present in the data)
+'''
+
+absurd_values = np.where((temprature< -5) | (temprature > 50))
+print("Here are indices of absurd values:",absurd_values)
+
+print()
+
+'''
+9. What strategy would you use to handle such data points?
+'''
+
+
+print()
+
+'''
+10. Find and print the indexes of all the outlier(unusual) values present in
+the above dataset.
+'''
+print("Here are indices of all outliers in array:",absurd_values)
+
+print()
+
+'''
+11.Replace the outliers with an appropriate value.
+'''
+
+print()
+
+'''
+12. Find the average max temperature for the winter months in Jaipur.
+'''
+print(np.mean(temprature[:,:,:,1], axis=(1,2)))
